ba2159da96eec392c4804e2da5c396f0cb6cd859
SpyerNot <low_li_wen@students.edu.sg>
2024-10-16 11:36:11 +0800

    Update friendlist.py

--- pages/friendlist.py
+++ pages/friendlist.py
@@ -1,5 +1,3 @@
 import streamlit as st
 st.header("Your friendlist!!")
-with st.popover("Tom"):
-  st.markdown("Statistics for Tom")
-  st.image("Investigation-and-statistics-of-students-weekly-exercise-times")
+
